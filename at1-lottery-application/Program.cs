1400a76d6f672ede038786f175b04c1a0da7b115
Jasmine Sullivan <20147180@tafe.wa.edu.au>
2025-08-28 22:39:30 +0800

    Adding comment instruction plan/layout

---
+++ at1-lottery-application/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Lottery Sort and Search AT1 
+ * Jasmine Sullivan 
+ * 20147180
+ * Certificate IV - C# - NMTAFE
+ * 28/08/2025 - 00/00/0000 */
+
+
+// Display Welcome Message and Instructions
+
+// Cutomise Variables
+
+// User enters numbers
+
+// Validate if its a number and in range
+
+// Linear search to check for no duplicates
+
+// Store valid numbers
+
+// Generate the random lottery numbers
+
+// Sort the lottery numbers
+
+// Binary search to loop through the users numbers to see if it's in the lottery numbers
+
+// Count total matches
+
+// Display the results 
